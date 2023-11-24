fa2c9f0b8021ee08e4a7a23b24f3dabd4cde7afa
Loris Laurenti <120016236+redes19@users.noreply.github.com>
2023-11-24 14:58:17 +0100

    Add files via upload

---
+++ Classes/PersonnageDAO.php
@@ -0,0 +1,78 @@
+<?php
+class PersonnageDAO {
+private $db;
+
+    public function __construct($db){
+        $this->db = $db;
+    }
+
+    public function getPersonnage($id){
+        $req = $this->db->prepare('SELECT * FROM personnage WHERE id = :id');
+        $req->execute(array('id' => $id));
+        $donnees = $req->fetch();
+        return $donnees;
+    }
+
+    public function getPersonnageNiveau($id){
+        $req = $this->db->prepare('SELECT level FROM personnage WHERE id = :id');
+        $req->execute(array('id' => $id));
+        $donnees = $req->fetchAll()[0][0];
+        return $donnees;
+    }
+
+    public function getPersonnages(){
+        $personnages = array();
+        $req = $this->db->prepare('SELECT * FROM personnage');
+        $req->execute();
+        while ($donnees = $req->fetch()){
+            $personnages[] = $donnees;
+        }
+        return $personnages;
+    }
+
+    public function getLastPersonnageId(){
+        $req = $this->db->prepare('SELECT MAX(id) FROM personnage');
+        $req->execute();
+        $donnees = $req->fetch();
+        return $donnees;
+    }
+
+    public function addPersonnage($personnage){
+        $req = $this->db->prepare('INSERT INTO personnage(nom, pv, atk, def, exp, level, maxpv, maxdef, maxatk) VALUES(:name, :pv, :atk, :def, :exp, :level, :maxpv, :maxdef, :maxatk)');
+        $req->execute(array(
+            'name' => $personnage->getName(),
+            'pv' => $personnage->getPv(),
+            'atk' => $personnage->getAtk(),
+            'def' => $personnage->getDef(),
+            'exp' => $personnage->getExp(),
+            'level' => $personnage->getLevel(),
+            'maxpv' => $personnage->getMaxpv(),
+            'maxdef' => $personnage->getMaxdef(),
+            'maxatk' => $personnage->getMaxatk()
+        ));
+    }
+
+    public function updatePersonnage ($id, $personnage) {
+        $req = $this->db->prepare('UPDATE personnage SET nom = :name, pv = :pv, atk = :atk, def = :def, exp = :exp, level = :level WHERE id = :id');
+        $req->execute(array(
+            'name' => $personnage->getName(),
+            'pv' => $personnage->getPv(),
+            'atk' => $personnage->getAtk(),
+            'def' => $personnage->getDef(),
+            'exp' => $personnage->getExp(),
+            'level' => $personnage->getLevel(),
+            'id' => $id
+        ));
+        
+    }
+
+    public function deletePersonnage($id){
+        $sql = 'DELETE FROM personnage WHERE id = :id';
+        $requete = $this->db->prepare($sql);
+        $requete->execute(array('id' => $id));
+
+    }
+}
+    
+
+?>
